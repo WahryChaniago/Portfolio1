23ca692c728e7a81ee6d62f8fbc91a80f694519f
Wahri Harma <wahrychaniago98@gmail.com>
2024-05-30 16:57:41 +0700

    gasss

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toogle class active
+
+const navbarNav = document.querySelector ('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar utk menghilangkan navbar
+
+const hamburger = document.querySelector ('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
